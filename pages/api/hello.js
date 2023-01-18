6020da98669d5540aa417d682e364fdaf0b2715c
oboitsun <thewip3out@gmail.com>
2023-01-18 23:47:57 +0200

    new check

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,6 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import emailjs from "@emailjs/nodejs";
+i; //mport emailjs from "@emailjs/nodejs";
 export default async function handler(req, res) {
+  const emailjs = require("@emailjs/nodejs");
   const { name, email, budget, message } = req?.body || {};
   var data = {
     service_id: process.env.SERVICE_ID,
