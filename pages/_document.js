6d003b4c3fa486e859d57db4533d782ad8d1fb99
oboitsun <thewip3out@gmail.com>
2025-02-02 08:56:51 +0200

    added new gtm tags

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,11 @@ export default function Document() {
       <Head>
         <script
           dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-NTXJXL39');`,
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-K853GKPH');`,
           }}
         />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
@@ -19,14 +23,9 @@ export default function Document() {
       <body>
         <noscript
           dangerouslySetInnerHTML={{
-            __html: `<iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-NTXJXL39"
-            height="0"
-            width="0"
-            style="display:none;visibility:hidden"
-          ></iframe>`,
-          }}
-        ></noscript>
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K853GKPH"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          }}></noscript>
         <Main />
         <NextScript />
       </body>
